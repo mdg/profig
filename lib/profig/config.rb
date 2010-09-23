be5fbdd3d5601e91467cc9a5443db53d39b3dd38
Matthew Graham <mdg149@gmail.com>
2010-09-23 17:37:31 -0400

    changed yaml format to allow ordering
    also fixed config code to put config into ruby classes instead of
    leaving as loose dictionaries & arrays.
    items are now handled in the order they appear in the file, and the
    files are handled in alphabetical order.
    
    added support for handling an entire section at a time rather than only
    as individual items.  this will be better for things like apt-get where
    you can put in a whole list of what to update and do it in a single call
    rather than several individual calls

--- lib/profig/config.rb
+++ lib/profig/config.rb
@@ -3,24 +3,40 @@ require 'yaml'
 module Profig
 
 
-# Take an array of configs, and merge them into a single list
-def self.merge_config(confs)
-	merge = {}
-	confs.each do |c|
-		c.each do |name, items|
-			if merge.has_key? name:
-				merge[name].concat(items)
-			else
-				merge[name] = items
-			end
-		end
+# An individual piece of configuration
+class Item
+	attr_reader :name
+	attr_reader :opts
+
+	def initialize(name, opts)
+		@name = name
+		@opts = opts
 	end
-	return merge
+end
+
+# A group of configuration items that are of the same type and should
+# be handled at the same time
+class Section
+	attr_reader :type
+	attr_reader :items
+
+	def initialize(type)
+		@type = type
+		@items = []
+	end
+
+	def <<(item)
+		if not item.instance_of? Item
+			raise 'Unknown item type'
+		end
+		@items << item
+	end
 end
 
 
 def self.load_config_yaml(directory)
-	files = Dir.glob("#{directory}/*")
+	# do a stable ordering by alphabetical
+	files = Dir.glob("#{directory}/*").sort
 	confs = []
 	files.each do |f|
 		if File.file?(f)
@@ -31,9 +47,50 @@ def self.load_config_yaml(directory)
 end
 
 
+def self.yaml_items(yitems)
+	yitems.each do |item|
+		if item.instance_of? String
+			name = item
+			opts = nil
+		else
+			name = item.keys[0]
+			opts = item[name]
+		end
+		yield name, opts
+	end
+end
+
+def self.yaml_sections(yaml)
+	yaml.each do |doc|
+		if not doc.has_key? 'profig'
+			raise 'Invalid configuration format'
+		end
+
+		doc['profig'].each do |yaml_section|
+			type = yaml_section.keys[0]
+			items = yaml_section[type]
+			yield type, items
+		end
+	end
+end
+
+def self.yaml_to_sections(yaml)
+	section_acc = []
+	yaml_sections(yaml) do |type, yitems|
+		section = Section.new(type)
+		yaml_items(yitems) do |name, opts|
+			section << Item.new(name, opts).freeze
+		end
+		section.freeze
+		section_acc << section
+	end
+	return section_acc
+end
+
+
 def self.load_config(directory)
 	confs = load_config_yaml(directory)
-	return merge_config(confs)
+	return yaml_to_sections(confs)
 end
 
 
--- lib/profig/main.rb
+++ lib/profig/main.rb
@@ -4,37 +4,43 @@ require 'profig/ubuntu'
 module Profig
 
 
-def self.run_handler(handler, items)
+# Run all items one at a time
+def self.run_item_handler(handler, items)
 	items.each do |i|
-		if i.instance_of? String
-			name = i
-			opts = nil
-		else
-			name = i.keys[0]
-			opts = i[name]
-		end
-		handler.call(name, opts)
+		handler.call(i.name, i.opts)
 	end
 end
 
 
-def self.run(os, config)
+def self.run(os, sections)
 	users_groups = `id -G`.split()
 	if not users_groups.include? "0"
+		# this should be made an exception at some point
 		puts "You must be root to run profig."
 		exit
 	end
 
-	config.each do |name, items|
-		handler_name = "handle_#{os}_#{name}"
+	sections.each do |section|
+		type = section.type
+		section_handler_name = "handle_#{type}_section_for_#{os}"
+		#item_handler_name = "handle_#{type}_item_for_#{os}"
+		item_handler_name = "handle_#{os}_#{type}"
 
-		if not Profig.respond_to? handler_name
-			puts "No #{name} handler for #{os}"
+		# first check if there's a section handler for this type
+		# a section handler takes all items at once
+		if Profig.respond_to? section_handler_name
+			handler = method(section_handler_name)
+			handler.call(section.items)
+		# second, check if tere's a handler for individual items
+		# this type of handler will process items one at a time
+		elsif Profig.respond_to? item_handler_name
+			handler = method(item_handler_name)
+			run_item_handler(handler, section.items)
+		# if no handlers, complain about it and move on
+		else
+			puts "No #{type} handlers for #{os}"
 			next
 		end
-
-		handler = method(handler_name)
-		run_handler(handler, items)
 	end
 end
 
